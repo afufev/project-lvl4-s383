19c37c5d03ed773a5b61bdf8d444b725290bb2c3
afuf <alexanderfuf@gmail.com>
2019-02-21 14:17:41 +0300

    add new tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,11 @@
 import request from 'supertest';
 import matchers from 'jest-supertest-matchers';
 
-import { User, sequelize } from '../models';
+import { sequelize } from '../models';
 import app from '..';
 
-import {
-  user, unknownUser, updatedUser, updatedPassword, getCookie,
-} from './util';
-
-
 beforeAll(() => {
-  jasmine.addMatchers(matchers);
+  expect.extend(matchers);
 });
 
 describe('basic routes', () => {
@@ -36,145 +31,6 @@ describe('basic routes', () => {
     const res = await request.agent(server)
       .get('/wrong-path');
     expect(res).toHaveHTTPStatus(404);
-  });
-
-  afterEach((done) => {
-    server.close();
-    done();
-  });
-});
-
-describe('sessions', () => {
-  let server;
-
-  beforeEach(async () => {
-    server = app().listen();
-    await sequelize.sync({ force: true });
-    await request.agent(server)
-      .post('/users')
-      .send({ form: user });
-  });
-
-  it('GET /session/new', async () => {
-    const res = await request.agent(server)
-      .get('/session/new');
-    expect(res).toHaveHTTPStatus(200);
-  });
-
-  it('POST /session', async () => {
-    const res = await request.agent(server)
-      .post('/session')
-      .send({ form: user });
-    expect(res).toHaveHTTPStatus(302);
-  });
-
-  it('POST /session (errors)', async () => {
-    const res = await request.agent(server)
-      .post('/session')
-      .type('form')
-      .send({ form: unknownUser });
-    const returnPath = res.req.path;
-    expect(returnPath).toBe('/session');
-  });
-
-  it('DELETE /session', async () => {
-    const authRes = await request.agent(server)
-      .post('/session')
-      .type('form')
-      .send({ form: user });
-    expect(authRes).toHaveHTTPStatus(302);
-    const cookie = authRes.headers['set-cookie'];
-
-    const res = await request.agent(server)
-      .delete('/session')
-      .set('Cookie', cookie);
-    expect(res).toHaveHTTPStatus(302);
-  });
-
-  afterEach((done) => {
-    server.close();
-    done();
-  });
-});
-
-describe('users', () => {
-  let server;
-
-  beforeEach(async () => {
-    server = app().listen();
-    await sequelize.sync({ force: true });
-  });
-
-  it('GET /users/new', async () => {
-    const res = await request.agent(server)
-      .get('/users/new');
-    expect(res).toHaveHTTPStatus(200);
-  });
-
-  it('POST /users', async () => {
-    const res = await request.agent(server)
-      .post('/users')
-      .type('form')
-      .send({ form: user });
-    expect(res).toHaveHTTPStatus(302);
-  });
-
-  it('POST /users (errors)', async () => {
-    const res1 = await request.agent(server)
-      .post('/users')
-      .type('form')
-      .send({ form: null });
-    const returnPath = res1.req.path;
-    expect(returnPath).toBe('/users');
-
-    const res2 = await request.agent(server).get(returnPath);
-    expect(res2).toHaveHTTPStatus(200);
-  });
-
-  it('GET /account/profile/edit', async () => {
-    const cookie = await getCookie(server);
-
-    const res = await request.agent(server)
-      .get('/account/profile/edit')
-      .set('Cookie', cookie);
-    expect(res).toHaveHTTPStatus(200);
-  });
-
-  it('PATCH /account/profile', async () => {
-    const cookie = await getCookie(server);
-
-    const res = await request.agent(server)
-      .patch('/account/profile')
-      .type('form')
-      .send({ form: updatedUser })
-      .set('Cookie', cookie);
-    expect(res).toHaveHTTPStatus(302);
-  });
-
-  it('PATCH /account/settings/change_password', async () => {
-    updatedPassword.confirmPassword = updatedPassword.newPassword;
-    const cookie = await getCookie(server);
-
-    const res = await request.agent(server)
-      .patch('/account/settings/change_password')
-      .type('form')
-      .send({ form: updatedPassword })
-      .set('Cookie', cookie);
-    expect(res).toHaveHTTPStatus(302);
-  });
-
-  it('DELETE /account/settings/delete', async () => {
-    const cookie = await getCookie(server);
-
-    const res = await request.agent(server)
-      .delete('/account/settings/delete')
-      .set('Cookie', cookie);
-    expect(res).toHaveHTTPStatus(302);
-
-    const deletedUser = await User.findOne({
-      where: { email: updatedUser.email },
-    });
-    expect(deletedUser).toBeNull();
   });
 
   afterEach((done) => {
---
+++ __tests__/tasks.test.js
@@ -0,0 +1,182 @@
+import request from 'supertest';
+import matchers from 'jest-supertest-matchers';
+
+import {
+  User, Task, TaskStatus, sequelize,
+} from '../models';
+import app from '..';
+
+import { seedUsers, seedStatuses, prepareTasks } from './helpers';
+import { status, updatedStatus } from './__fixtures__/tasktatuses';
+import { firstTask, secondTask } from './__fixtures__/tasks';
+
+
+
+beforeAll(async () => {
+  expect.extend(matchers);
+});
+
+// describe('taskStatuses', () => {
+//   let server;
+//
+//   beforeAll(async () => {
+//     await sequelize.sync({ force: true });
+//     await seedUsers();
+//   });
+//
+//   beforeEach(async () => {
+//     server = app().listen();
+//   });
+//
+//   it('GET /taskStatuses/new', async () => {
+//     const res = await request.agent(server)
+//       .get('/taskStatuses/new');
+//     expect(res).toHaveHTTPStatus(200);
+//   });
+//
+//   it('POST /taskStatuses', async () => {
+//     const res = await request.agent(server)
+//       .post('/taskStatuses')
+//       .send({ form: status });
+//     expect(res).toHaveHTTPStatus(302);
+//   });
+//
+//   it('GET /taskStatuses/:id/settings', async () => {
+//     const { id } = await TaskStatus.findOne({ where: { name: `${status.name}` } });
+//     const res = await request.agent(server)
+//       .get(`/taskStatuses/${id}/settings`);
+//     expect(res).toHaveHTTPStatus(200);
+//   });
+//
+//   it('PATCH /taskStatuses/:id', async () => {
+//     const { id } = await TaskStatus.findOne({ where: { name: `${status.name}` } });
+//     const res = await request.agent(server)
+//       .patch(`/taskStatuses/${id}`)
+//       .type('form')
+//       .send({ form: updatedStatus });
+//     expect(res).toHaveHTTPStatus(302);
+//   });
+//
+//   it('DELETE /taskStatuses/:id/delete', async () => {
+//     const { id } = await TaskStatus.findOne({ where: { name: `${updatedStatus.name}` } });
+//     const res = await request.agent(server)
+//       .delete(`/taskStatuses/${id}/delete`);
+//     expect(res).toHaveHTTPStatus(302);
+//     const deletedStatus = await TaskStatus.findByPk(id);
+//     expect(deletedStatus).toBeNull();
+//   });
+//
+//   afterEach((done) => {
+//     server.close();
+//     done();
+//   });
+// });
+
+// describe('tasks', async () => {
+//   beforeAll(async () => {
+//     await sequelize.sync({ force: true });
+//     await seedUsers();
+//     await seedStatuses();
+//   });
+//
+//   let server;
+//   const updatedTask = { ...secondTask, id: firstTask.id };
+//
+//   beforeEach(async () => {
+//     server = app().listen();
+//   });
+//
+//   it('GET /tasks/new', async () => {
+//     const res = await request.agent(server)
+//       .get('/tasks/new');
+//     expect(res).toHaveHTTPStatus(200);
+//   });
+//
+//   it('POST /tasks', async () => {
+//     const res = await request.agent(server)
+//       .post('/tasks')
+//       .send({ form: firstTask });
+//     expect(res).toHaveHTTPStatus(302);
+//   });
+//
+//   it('GET /tasks/:id', async () => {
+//     const res = await request.agent(server)
+//       .get(`/tasks/${firstTask.id}`);
+//     expect(res).toHaveHTTPStatus(200);
+//   });
+//
+//   it('GET /tasks/:id/settings', async () => {
+//     const res = await request.agent(server)
+//       .get(`/tasks/${firstTask.id}/settings`);
+//     expect(res).toHaveHTTPStatus(200);
+//   });
+//
+//   it('PATCH /tasks/:id', async () => {
+//     const res = await request.agent(server)
+//       .patch(`/tasks/${updatedTask.id}`)
+//       .type('form')
+//       .send({ form: updatedTask });
+//     expect(res).toHaveHTTPStatus(302);
+//   });
+//
+//   it('DELETE /tasks/:id/delete', async () => {
+//     const res = await request.agent(server)
+//       .delete(`/tasks/${updatedTask.id}/delete`);
+//     expect(res).toHaveHTTPStatus(302);
+//     const deletedTask = await Task.findByPk(updatedTask.id);
+//     expect(deletedTask).toBeNull();
+//   });
+//
+//   afterEach(async (done) => {
+//     server.close();
+//     done();
+//   });
+// });
+
+describe('tasks table', async () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true });
+    await seedUsers();
+    await seedStatuses();
+    await prepareTasks();
+  });
+
+  let server;
+
+  beforeEach(async () => {
+    server = app().listen();
+  });
+
+  // it('all tasks', async () => {
+  //   const res = await request.agent(server)
+  //     .get('/tasks');
+  //   expect(res).toHaveHTTPStatus(200);
+  // });
+  //
+  // it('simple query', async () => {
+  //   // const query = {
+  //   //   limit: 4,
+  //   //   offset: 0,
+  //   //   order: [[Task, 'updatedAt']],
+  //   //   statusId: 1,
+  //   //   assigneeId: 1,
+  //   //   creatorId: 2,
+  //   // };
+  //   const res = await request.agent(server)
+  //     .get('/tasks')
+  //     .query({ statusId: 1, assigneeId: 1, creatorId: 2 });
+  //   expect(res).toHaveHTTPStatus(200);
+  // });
+
+  it('by tags', async () => {
+    const res = await request.agent(server)
+      .get('/tasks')
+      .query({ tagsQuery: '#firstTag#secondTag' });
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  afterEach(async (done) => {
+    server.close();
+    done();
+  });
+});
