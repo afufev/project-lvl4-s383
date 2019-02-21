8a7e3981fa8eb02230ccebb741c9398b2d066907
afuf <alexanderfuf@gmail.com>
2019-02-21 14:12:50 +0300

    rename util to helpers

---
+++ __tests__/helpers/index.js
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import {
+  User, Task, TaskStatus, Tag,
+} from '../../models';
+import { user, usersArr } from '../__fixtures__/users';
+import { statusArr } from '../__fixtures__/tasktatuses';
+import { taskArr } from '../__fixtures__/tasks';
+import { tagArr } from '../__fixtures__/tags';
+
+
+export const getCookie = async (server) => {
+  const authRes = await request.agent(server)
+    .post('/users')
+    .send({ form: user });
+  return authRes.headers['set-cookie'];
+};
+
+export const seedUsers = () => User.bulkCreate(usersArr);
+
+export const seedStatuses = () => TaskStatus.bulkCreate(statusArr);
+
+export const prepareTasks = async () => {
+  await Task.bulkCreate(taskArr);
+  await Tag.bulkCreate(tagArr);
+  const firstTask = await Task.findByPk(1);
+  const secondTask = await Task.findByPk(2);
+  const thirdTask = await Task.findByPk(3);
+  const firstTagId = 1;
+  const secondTagId = 2;
+  await firstTask.setTags([firstTagId, secondTagId]);
+  await secondTask.setTags(firstTagId);
+  await thirdTask.setTags(secondTagId);
+};
