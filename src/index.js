a0e35f56cf84864b3a830ac10695e48f1de59fe6
afuf <alexanderfuf@gmail.com>
2019-02-27 11:26:51 +0300

    split js and css files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 /* eslint-disable*/
 import 'bootstrap';
+import 'jquery';
+import 'jquery-ujs';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'open-iconic/font/css/open-iconic-bootstrap.css';
+//import '@fortawesome/fontawesome-free/css/all.min.css';
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,17 +1,65 @@
+// const webpack = require('webpack');
+// const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//
+// module.exports = {
+//   mode: process.env.NODE_ENV || 'development',
+//   entry: {
+//     main: ['./src/index.js'],
+//   },
+//   output: {
+//     path: path.join(__dirname, 'public', 'assets'),
+//     filename: 'main.js',
+//     publicPath: '/public/assets/',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: 'babel-loader',
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new webpack.ProvidePlugin({
+//       $: 'jquery',
+//       jQuery: 'jquery',
+//       'window.jQuery': 'jquery',
+//     }),
+//     new MiniCssExtractPlugin({
+//       filename: 'bundle.css',
+//     }),
+//   ],
+// };
 import path from 'path';
 import webpack from 'webpack';
 import autoprefixer from 'autoprefixer';
+import precss from 'precss';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
   mode: process.env.NODE_ENV || 'development',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles', test: /\.css$/, chunks: 'all', enforce: true,
+        },
+      },
+    },
+  },
   entry: {
     main: ['./src/index.js'],
-    vendor: ['jquery', 'jquery-ujs'],
   },
   output: {
     path: path.join(__dirname, 'public', 'assets'),
     filename: '[name].js',
-    publicPath: '/public/assets/',
+    publicPath: '/assets/',
   },
   module: {
     rules: [
@@ -22,19 +70,26 @@ export default {
       },
       {
         test: /\.css$/,
+        // exclude: /node_modules/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
           'css-loader',
-          // 'sass-loader',
+          'sass-loader',
           {
             loader: 'postcss-loader',
-            options: { plugins: [autoprefixer] },
+            options: { plugins: [precss, autoprefixer] },
           },
         ],
+      },
+      {
+        test: /\.(ttf|eot|otf|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader',
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin('[name].css'),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
