cb7d048af341573d21656f7804a44bd7bfd33739
afuf <alexanderfuf@gmail.com>
2019-02-28 15:05:52 +0300

    add id to workaround sequelize bug

--- models/TaskTag.js
+++ models/TaskTag.js
@@ -1,5 +1,11 @@
 export default (sequelize, DataTypes) => {
   const TaskTag = sequelize.define('TaskTag', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
     taskId: DataTypes.INTEGER,
     tagId: DataTypes.INTEGER,
   }, {});
